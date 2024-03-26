db777a7af02f842fdd499c78ba9c996179dd1e08
tahzeebahmed99 <tahzeeb2g@gmail.com>
2024-03-27 02:18:33 +0600

    Data summary and pagination on product list page completed

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -2,6 +2,8 @@ from django.views import generic
 from django.shortcuts import render
 import logging
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 from product.models import Variant, Product, ProductVariantPrice
 
 
@@ -18,11 +20,24 @@ class CreateProductView(generic.TemplateView):
 
 def list_product(request):
     products = []
+
     for product in Product.objects.all():
         product_variant_prices = ProductVariantPrice.objects.filter(
             product=product)
         product = product.__dict__
         product['variants'] = product_variant_prices
         products.append(product)
+
+    paginator = Paginator(products, 2)
+    page_number = request.GET.get('page')
+
+    try:
+        products = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        products = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        products = paginator.page(paginator.num_pages)
 
     return render(request, 'products/list.html', {'products': products})
